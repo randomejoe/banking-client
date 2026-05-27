32efa65f85afab0c9e64d72eba69f0dce8d3e9d7
randomejoe <144443137+randomejoe@users.noreply.github.com>
2026-05-27 14:30:49 +0200

    Set base path for banking client in Vite config

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ const dirname =
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   // plugins: [vue(), vueDevTools()],
+  base: '/banking-client/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
