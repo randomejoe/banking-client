ef20252ace0ba963753b7af36ecef2ad13bb4ffe
randomejoe <144443137+randomejoe@users.noreply.github.com>
2026-05-27 14:39:03 +0200

    Change router history mode to use hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuthStore } from '../stores/auth.js'
 
 import LoginPage from '../components/pages/LoginPage/LoginPage.vue'
@@ -56,7 +56,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
